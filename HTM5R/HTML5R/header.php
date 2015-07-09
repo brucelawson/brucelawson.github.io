22567655d2888df45cd7f9b408455ea508e4c560
Bruce Lawson <brucelawson@Bruces-MacBook-Air.local>
2015-07-09 09:20:42 +0100

    viewport

--- HTM5R/HTML5R/header.php
+++ HTM5R/HTML5R/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang=en>
 <head>
-<meta charset=utf-8> <!-- simplified version; works on legacy browsers -->
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width, initial-scale=1">
 <title>
 <?php bloginfo('name'); ?>
 <?php if ( is_single() ) { ?>
--- HTM5R/HTML5R/searchform.php
+++ HTM5R/HTML5R/searchform.php
@@ -1,4 +1,5 @@
 <form method=get id=searchform role=search action="<?php echo $_SERVER['PHP_SELF']; ?>">
-<input name=s id=s type="search" value="<?php echo wp_specialchars($s, 1); ?>" title="Search" placeholder="Search this site">
+<input name=s id=s type="search" value="<?php echo wp_specialchars($s, 1); ?>" 
+title="Search" placeholder="Search this site">
 <input type="submit" id="searchsubmit" value="Search" />
 </form>

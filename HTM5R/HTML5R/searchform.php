c3b607e4c485d3d1016d73ad109d32bd4f668850
Bruce Lawson <bruce@brucelawson.co.uk>
2021-06-14 12:03:01 +0100

    Chonks

--- HTM5R/HTML5R/searchform.php
+++
@@ -1,5 +0,0 @@
-<form method=get id=searchform role=search action="<?php echo $_SERVER['PHP_SELF']; ?>">
-<input name=s id=s type="search" value="<?php echo wp_specialchars($s, 1); ?>" 
-title="Search" placeholder="Search this site">
-<input type="submit" id="searchsubmit" value="Search" />
-</form>
